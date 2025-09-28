5ef6001ba35f465d861051d98fbe4446a8565661
opsec-ee <opsec.ee@pm.me>
2025-09-28 07:07:29 -0400

    commit

--- RTKA-ML/code/rtka_graph.h
+++ RTKA-ML/code/rtka_graph.h
@@ -2,6 +2,10 @@
  * File: rtka_graph.h
  * Copyright (c) 2025 - H.Overman opsec.ee@pm.me
  * 
+ * PROPRIETARY AND CONFIDENTIAL
+ * This file is proprietary to H. Overman and may not be reproduced,
+ * distributed, or used without explicit written permission.
+ *
  * RTKA Graph Framework - PageRank-inspired Confidence Propagation
  */
 
