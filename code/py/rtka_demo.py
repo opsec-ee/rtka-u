fbf36dadac165a4981783c2d697e478c7387344d
opsec-ee <opsec.ee@pm.me>
2025-09-28 07:37:12 -0400

    commit

--- code/py/rtka_demo.py
+++ code/py/rtka_demo.py
@@ -6,7 +6,11 @@ Advanced Recursion Python Implementation (v1.3)
 Copyright (c) 2025 - H.Overman <opsec.ee@pm.me>
 DOI: https://doi.org/10.5281/zenodo.17148691
 ORCHID: https://orcid.org/0009-0007-9737-762X
-
+ * 
+ * PROPRIETARY AND CONFIDENTIAL
+ * This file is proprietary to H. Overman and may not be reproduced,
+ * distributed, or used without explicit written permission.
+ *
 """
 
 import numpy as np
