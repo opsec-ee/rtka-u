721b1512a5803660c46cbe89c3b045be8ff0e823
opsec-ee <opsec.ee@pm.me>
2025-09-28 07:03:15 -0400

    commit

--- RTKA-ML/code/rtka_astar.c
+++ RTKA-ML/code/rtka_astar.c
@@ -2,6 +2,10 @@
  * File: rtka_astar.c
  * Copyright (c) 2025 - H.Overman opsec.ee@pm.me
  * 
+ * PROPRIETARY AND CONFIDENTIAL
+ * This file is proprietary to H. Overman and may not be reproduced,
+ * distributed, or used without explicit written permission.
+ *
  * RTKA A* Search Implementation
  * 
  * CHANGELOG:
